90b229c8c3c3ce0d85e485cc1eb691f76b89aa70
Rafka <fw.ge-mu.930@jb4.so-net.ne.jp>
2017-12-21 20:27:18 +0900

    Initial commit.

--- MKLSharp/Blas1.cpp
+++ MKLSharp/Blas1.cpp
@@ -2,5 +2,5 @@
 
 #include "stdafx.h"
 
-#include "MKLSharp.h"
+#include "Blas1.h"
 
---
+++ MKLSharp/Blas1.h
@@ -0,0 +1,10 @@
+// MKLSharp.h
+
+#pragma once
+
+using namespace System;
+
+namespace MKLSharp {
+
+  public ref class Class1 {};
+}
