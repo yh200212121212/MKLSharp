0b075ada3ee29e3a56d153f033d9ef8c6931d3b0
Rafka <fw.ge-mu.930@jb4.so-net.ne.jp>
2017-12-22 01:57:57 +0900

    Add ?gemv functions.

--- MKLSharp/Blas2.cpp
+++ MKLSharp/Blas2.cpp
@@ -29,4 +29,29 @@ namespace MKLSharp {
     ptr_x = nullptr;
     ptr_y = nullptr;
   }
+
+  void Blas2::sgemv(CBlasLayout Layout, CBlasTranspose Trans,
+                    long m, long n, float alpha, array<float>^ a, long lda,
+                    array<float>^ x, long incX, float beta, array<float>^ y, long incY) {
+    pin_ptr<float> ptr_a = &a[0];
+    pin_ptr<float> ptr_x = &x[0];
+    pin_ptr<float> ptr_y = &y[0];
+    cblas_sgemv((CBLAS_LAYOUT)Layout, (CBLAS_TRANSPOSE)Trans,
+                m, n, alpha, ptr_a, lda, ptr_x, incX, beta, ptr_y, incY);
+    ptr_a = nullptr;
+    ptr_x = nullptr;
+    ptr_y = nullptr;
+  }
+  void Blas2::dgemv(CBlasLayout Layout, CBlasTranspose Trans,
+                    long m, long n, double alpha, array<double>^ a, long lda,
+                    array<double>^ x, long incX, double beta, array<double>^ y, long incY) {
+    pin_ptr<double> ptr_a = &a[0];
+    pin_ptr<double> ptr_x = &x[0];
+    pin_ptr<double> ptr_y = &y[0];
+    cblas_dgemv((CBLAS_LAYOUT)Layout, (CBLAS_TRANSPOSE)Trans,
+                m, n, alpha, ptr_a, lda, ptr_x, incX, beta, ptr_y, incY);
+    ptr_a = nullptr;
+    ptr_x = nullptr;
+    ptr_y = nullptr;
+  }
 }
--- MKLSharp/Blas2.h
+++ MKLSharp/Blas2.h
@@ -12,5 +12,12 @@ namespace MKLSharp {
                       long m, long n, long kl, long ku,
                       double alpha, array<double>^ a, long lda,
                       array<double>^ x, long incX, double beta, array<double>^ y, long incY);
+
+    static void sgemv(CBlasLayout Layout, CBlasTranspose Trans,
+                      long m, long n, float alpha, array<float>^ a, long lda,
+                      array<float>^ x, long incX, float beta, array<float>^ y, long incY);
+    static void dgemv(CBlasLayout Layout, CBlasTranspose Trans,
+                      long m, long n, double alpha, array<double>^ a, long lda,
+                      array<double>^ x, long incX, double beta, array<double>^ y, long incY);
   };
 }
