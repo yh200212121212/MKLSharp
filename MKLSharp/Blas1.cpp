10c957f661c4d994ea4e2e55595aca16970f9714
Rafka <fw.ge-mu.930@jb4.so-net.ne.jp>
2017-12-21 22:51:47 +0900

    Add ?axpy functions.

--- MKLSharp/Blas1.cpp
+++ MKLSharp/Blas1.cpp
@@ -9,4 +9,25 @@ namespace MKLSharp {
     ptr_x = nullptr;
     return res;
   }
+  double Blas1::dasum(int n, array<double>^ x, int incX) {
+    pin_ptr<double> ptr_x = &x[0];
+    auto res = cblas_dasum(n, ptr_x, incX);
+    ptr_x = nullptr;
+    return res;
+  }
+
+  void Blas1::saxpy(int n, float a, array<float>^ x, int incX, array<float>^ y, int incY) {
+    pin_ptr<float> ptr_x = &x[0];
+    pin_ptr<float> ptr_y = &y[0];
+    cblas_saxpy(n, a, ptr_x, incX, ptr_y, incY);
+    ptr_x = nullptr;
+    ptr_y = nullptr;
+  }
+  void Blas1::daxpy(int n, double a, array<double>^ x, int incX, array<double>^ y, int incY) {
+    pin_ptr<double> ptr_x = &x[0];
+    pin_ptr<double> ptr_y = &y[0];
+    cblas_daxpy(n, a, ptr_x, incX, ptr_y, incY);
+    ptr_x = nullptr;
+    ptr_y = nullptr;
+  }
 }
--- MKLSharp/Blas1.h
+++ MKLSharp/Blas1.h
@@ -3,9 +3,12 @@
 using namespace System;
 
 namespace MKLSharp {
-
   public ref class Blas1 {
   public:
     static float sasum(int n, array<float>^ x, int incX);
+    static double dasum(int n, array<double>^ x, int incX);
+
+    static void saxpy(int n, float a, array<float>^ x, int incX, array<float>^ y, int incY);
+    static void daxpy(int n, double a, array<double>^ x, int incX, array<double>^ y, int incY);
   };
 }
