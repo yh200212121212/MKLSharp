acaeeb8600ce5793acdf1d30417157a29e349558
Rafka <fw.ge-mu.930@jb4.so-net.ne.jp>
2017-12-22 01:52:23 +0900

    Add ?gbmv functions.

--- MKLSharp/Blas2.h
+++ MKLSharp/Blas2.h
@@ -3,4 +3,14 @@
 using namespace System::Runtime::InteropServices;
 
 namespace MKLSharp {
+  public ref class Blas2 {
+    static void sgbmv(CBlasLayout Layout, CBlasTranspose Trans,
+                      long m, long n, long kl, long ku,
+                      float alpha, array<float>^ a, long lda,
+                      array<float>^ x, long incX, float beta, array<float>^ y, long incY);
+    static void dgbmv(CBlasLayout Layout, CBlasTranspose Trans,
+                      long m, long n, long kl, long ku,
+                      double alpha, array<double>^ a, long lda,
+                      array<double>^ x, long incX, double beta, array<double>^ y, long incY);
+  };
 }
