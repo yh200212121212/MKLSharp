df247c04f0ae35a2fbc8af1bec2c01eeda1b9c95
Rafka <fw.ge-mu.930@jb4.so-net.ne.jp>
2017-12-22 21:13:47 +0900

    Add ?getrs functions.

--- MKLSharp/general.cpp
+++ MKLSharp/general.cpp
@@ -23,4 +23,29 @@ namespace MKLSharp {
     ptr_i = nullptr;
     return res;
   }
+
+  __int64 Lapack::sgetrs(LapackLayout Layout, char trans,
+                         __int64 n, __int64 nrhs, array<float>^ a, __int64 lda,
+                         array<__int64>^ ipiv, array<float>^ b, __int64 ldb) {
+    pin_ptr<float> ptr_a = &a[0];
+    pin_ptr<__int64> ptr_i = &ipiv[0];
+    pin_ptr<float> ptr_b = &b[0];
+    auto res = LAPACKE_sgetrs((int)Layout, trans, n, nrhs, ptr_a, lda, ptr_i, ptr_b, ldb);
+    ptr_a = nullptr;
+    ptr_i = nullptr;
+    ptr_b = nullptr;
+    return res;
+  }
+  __int64 Lapack::dgetrs(LapackLayout Layout, char trans,
+                         __int64 n, __int64 nrhs, array<double>^ a, __int64 lda,
+                         array<__int64>^ ipiv, array<double>^ b, __int64 ldb) {
+    pin_ptr<double> ptr_a = &a[0];
+    pin_ptr<__int64> ptr_i = &ipiv[0];
+    pin_ptr<double> ptr_b = &b[0];
+    auto res = LAPACKE_dgetrs((int)Layout, trans, n, nrhs, ptr_a, lda, ptr_i, ptr_b, ldb);
+    ptr_a = nullptr;
+    ptr_i = nullptr;
+    ptr_b = nullptr;
+    return res;
+  }
 }
--- MKLSharp/general.h
+++ MKLSharp/general.h
@@ -10,5 +10,12 @@ namespace MKLSharp {
                           array<float>^ a, __int64 lda, [Out]array<__int64>^% ipiv);
     static __int64 dgetrf(LapackLayout Layout, __int64 m, __int64 n,
                           array<double>^ a, __int64 lda, [Out]array<__int64>^% ipiv);
+
+    static __int64 sgetrs(LapackLayout Layout, char trans,
+                          __int64 n, __int64 nrhs, array<float>^ a, __int64 lda,
+                          array<__int64>^ ipiv, array<float>^ b, __int64 ldb);
+    static __int64 dgetrs(LapackLayout Layout, char trans,
+                          __int64 n, __int64 nrhs, array<double>^ a, __int64 lda,
+                          array<__int64>^ ipiv, array<double>^ b, __int64 ldb);
   };
 }
