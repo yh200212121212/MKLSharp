cdb704deac35364eded45a2f6b76574e778b09e1
Rafka <fw.ge-mu.930@jb4.so-net.ne.jp>
2017-12-22 20:42:09 +0900

    Add ?getrf functions.

--- MKLSharp/general.cpp
+++ MKLSharp/general.cpp
@@ -3,4 +3,24 @@
 #include "general.h"
 
 namespace MKLSharp {
+  __int64 Lapack::sgetrf(LapackLayout Layout, __int64 m, __int64 n,
+                         array<float>^ a, __int64 lda, [Out]array<__int64>^% ipiv) {
+    pin_ptr<float> ptr_a = &a[0];
+    ipiv = gcnew array<__int64>((int)Math::Max(1LL, Math::Min(m, n)));
+    pin_ptr<__int64> ptr_i = &ipiv[0];
+    auto res = LAPACKE_sgetrf((int)Layout, m, n, ptr_a, lda, ptr_i);
+    ptr_a = nullptr;
+    ptr_i = nullptr;
+    return res;
+  }
+  __int64 Lapack::dgetrf(LapackLayout Layout, __int64 m, __int64 n,
+                         array<double>^ a, __int64 lda, [Out]array<__int64>^% ipiv) {
+    pin_ptr<double> ptr_a = &a[0];
+    ipiv = gcnew array<__int64>((int)Math::Max(1LL, Math::Min(m, n)));
+    pin_ptr<__int64> ptr_i = &ipiv[0];
+    auto res = LAPACKE_dgetrf((int)Layout, m, n, ptr_a, lda, ptr_i);
+    ptr_a = nullptr;
+    ptr_i = nullptr;
+    return res;
+  }
 }
--- MKLSharp/general.h
+++ MKLSharp/general.h
@@ -1,8 +1,14 @@
 #pragma once
 
+using namespace System;
 using namespace System::Runtime::InteropServices;
 
 namespace MKLSharp {
   public ref class Lapack {
+  public:
+    static __int64 sgetrf(LapackLayout Layout, __int64 m, __int64 n,
+                          array<float>^ a, __int64 lda, [Out]array<__int64>^% ipiv);
+    static __int64 dgetrf(LapackLayout Layout, __int64 m, __int64 n,
+                          array<double>^ a, __int64 lda, [Out]array<__int64>^% ipiv);
   };
 }
