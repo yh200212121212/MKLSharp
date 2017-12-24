f286486a495c59ab2c593531ebcb26acc67c528d
Rafka <fw.ge-mu.930@jb4.so-net.ne.jp>
2017-12-24 16:53:51 +0900

    Add ?syrfsx functions.

--- MKLSharp/Lapack.h
+++ MKLSharp/Lapack.h
@@ -452,6 +452,25 @@ namespace MKLSharp {
                           array<double>^ b, int ldb,
                           array<double>^ x, int ldx, 
                           [Out]array<double>^% fErr, [Out]array<double>^% bErr);
+
+    static __int64 ssyrfsx(LapackLayout Layout, LapackUpLo UpLo, LapackEquil Equed,
+                           int n, int nrhs, array<float>^ a, int lda,
+                           array<float>^ af, int ldaf, array<__int64>^ ipiv,
+                           array<float>^ s, array<float>^ b, int ldb,
+                           array<float>^ x, int ldx,
+                           [Out]float% rCond, [Out]array<float>^% bErr, 
+                           int nErrBnds, [Out]array<float>^% errBndsNorm, 
+                           [Out]array<float>^% errBndsComp,
+                           int nParams, array<float>^ params);
+    static __int64 dsyrfsx(LapackLayout Layout, LapackUpLo UpLo, LapackEquil Equed,
+                           int n, int nrhs, array<double>^ a, int lda,
+                           array<double>^ af, int ldaf, array<__int64>^ ipiv,
+                           array<double>^ s, array<double>^ b, int ldb,
+                           array<double>^ x, int ldx,
+                           [Out]double% rCond, [Out]array<double>^% bErr, 
+                           int nErrBnds, [Out]array<double>^% errBndsNorm, 
+                           [Out]array<double>^% errBndsComp,
+                           int nParams, array<double>^ params);
   #pragma endregion
   };
 }
